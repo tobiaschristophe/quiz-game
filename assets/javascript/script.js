20aad37d901b0b5d5cb4201e5764d10d096e695c
Tobias Christophe <tobias.christophe@gmail.com>
2023-07-20 10:34:51 +0000

    Add questions

---
+++ assets/javascript/script.js
@@ -0,0 +1,31 @@
+const quizData = [
+    {
+      question: "Leonardo Dicaprio won an Oscar for which movie?",
+      options: ["Titanic", "Catch me if you can", "The Revenant", "Blood Diamond"],
+      answer: "The Revenant",
+      image: "assets/images/leonardo.jpeg",
+    },
+
+    {
+        question: "What´s the name of Batmans sidekick?",
+        options: ["Bob", "Albert", "James", "Robin"],
+        answer: "Robin",
+        image: "assets/images/robin.jpg",
+      },
+
+      {
+        question: "Who plays the main character in The mask of Zorro?",
+        options: ["Antonio Banderas", "Danny Devito", "Ryan Gosling", "Ryan Reynolds"],
+        answer: "Antonio Banderas",
+        image: "assets/images/antonio.jpg",
+      },
+
+      {
+        question: "Which movie has the highest ranking on IMDB?",
+        options: ["Shrek 2", "The Shawshank Redemption", "Avatar", "Shindlers List"],
+        answer: "The Shawshank Redemption",
+        image: "assets/images/shawshank.jpg",
+      },
+
+      
+
